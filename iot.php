372c339730202a8d8340048f51231366a885f8d6
James-Chao <jameschao.qci@gmail.com>
2020-03-25 11:03:17 +0800

    iot.php change db information

--- iot.php
+++ iot.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_server="10.243.22.39";//資料庫連結網址
+$db_server="10.243.22.243";//資料庫連結網址
 $db_name="reactor";//資料庫名稱
-$db_user="iot";//資料庫帳號
-$db_pass="iot1234";//資料庫密碼
+$db_user="root";//資料庫帳號
+$db_pass="i2i2##i2";//資料庫密碼
 
 //mysql
 $link=mysql_connect("$db_server","$db_user","$db_pass");
