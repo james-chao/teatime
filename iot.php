913a2a494ef2e9de9d22dea66d7d90a742a29326
James-Chao <jameschao.qci@gmail.com>
2020-03-25 11:05:14 +0800

    second change

--- iot.php
+++ iot.php
@@ -10,14 +10,11 @@ $link=mysql_connect("$db_server","$db_user","$db_pass");
 mysql_query("SET NAMES 'utf8'");
 mysql_select_db("$db_name",$link);
 
-
-
 $strSql="SELECT * from item";
 
 $data_rows=mysql_query($strSql,$link);
 if($rows = mysql_fetch_array($data_rows))
 {
 	print_r($rows);
-
 }
 echo mysql_error();
